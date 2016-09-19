af9acb98039726c37240ba2ffc8b65fe3c3ee3e7
Chris Dermody <chrisdermody1@gmail.com>
2016-09-19 02:11:50 +0100

    map centres on stepaside for now. on driver change, drops pin on map

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,19 +1,32 @@
-
-
+var map;
 
 var loadMap = function(){
   var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-      center: {lat: 44.540, lng: -78.546},
+  map = new google.maps.Map(mapDiv, {
+      center: {lat: 53.252751, lng: -6.213784 },
       zoom: 8
   });
 }
 
+var store = firebase.database().ref('stores/borza');
+var drivers = firebase.database().ref('stores/borza/drivers');
 
 var watchDriver = function(){
-	var driver = firebase.database().ref('stores/borza');
-    driver.on('child_changed', function(data) {
+    drivers.on('child_changed', function(data) {
       console.log(data.val());
+      var driver = data.val();
+      var latlng = new google.maps.LatLng(driver.position.lat, driver.position.long);
+      updatePin(latlng)
+    });
+}
+
+
+var updatePin = function(latLng){
+	console.log('updating pin');
+	var marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
     });
 }
 
