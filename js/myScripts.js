d6fb2ca0f23cc248ff4a5b71d1af716eaac409e8
Chris Dermody <chrisdermody1@gmail.com>
2016-09-19 20:49:18 +0100

    updated code to work with everything, should be A1

--- js/myScripts.js
+++ js/myScripts.js
@@ -15,58 +15,67 @@ var loadMap = function(){
   });
 }
 
-var store;
-var storeRef = firebase.database().ref('stores/borza');
-var drivers = firebase.database().ref('stores/borza/drivers');
-
-storeRef.on('value', function(snapshot) {
-  store = snapshot.val();
-  console.log(snapshot.val());
-  watchDriver();
-});
-
-var watchDriver = function(){
-    drivers.on('value', function(data) {
-      console.log(data.val());
-      var driversArray = data.val();
-
-      for (var i = driversArray.length - 1; i >= 0; i--) {
-      	driversArray[i]
-      	var latlng = new google.maps.LatLng(driversArray[i].position.lat, driversArray[i].position.long);
-	      calcRoute(driversArray[i])
-	      updatePin(latlng)
-      }
-      
-    });
-}
+var startMagic = function(){
 
 
-var updatePin = function(latLng){
-	console.log('updating pin');
-	var marker = new google.maps.Marker({
-        map: map,
-        animation: google.maps.Animation.DROP,
-        position: latLng
-    });
-}
+	var store;
+	var storeRef = firebase.database().ref('stores/borza');
+	var drivers = firebase.database().ref('stores/borza/drivers');
+
+	storeRef.on('value', function(snapshot) {
+	  store = snapshot.val();
+	  console.log(snapshot.val());
+	  watchDrivers();
+	});
+
+	var watchDrivers = function(){
+	    drivers.on('value', function(data) {
+	      console.log(data.val());
+	      var driversObj = data.val();
+
+	      for (var driver in driversObj) {
+				  if (driversObj.hasOwnProperty(driver)) {
+				    console.log(" -> " + JSON.stringify(driversObj[driver]));
+
+						var latlng = new google.maps.LatLng(driversObj[driver].latitude, driversObj[driver].longitude);
+						calcRoute(driversObj[driver])
+						updatePin(latlng)
+				  }
+				}
+	    });
+	}
 
 
-function calcRoute(driver) {
-	console.log('running calcRoute', driver, store);
-  var start = driver.position.lat + ", "+ driver.position.long;
-  var end = store.location.lat + ", " + store.location.long;
-  console.log('start:', start, "end:", end)
-  var request = {
-    origin: start,
-    destination: end,
-    travelMode: 'DRIVING'
-  };
-  directionsService.route(request, function(result, status) {
-  	console.log('result:', result, status)
-    if (status == 'OK') {
-    	var point = result.routes[ 0 ].legs[ 0 ]
-      console.log(point.duration.text)
-    }
-  });
+	var updatePin = function(latLng){
+		console.log('updating pin');
+		var marker = new google.maps.Marker({
+	        map: map,
+	        position: latLng
+	    });
+	}
+
+
+	function calcRoute(driver) {
+		console.log('running calcRoute', driver, store);
+	  var start = driver.latitude + ", "+ driver.longitude;
+	  var end = store.location.lat + ", " + store.location.long;
+	  console.log('start:', start, "end:", end)
+	  var request = {
+	    origin: start,
+	    destination: end,
+	    travelMode: 'DRIVING'
+	  };
+	  directionsService.route(request, function(result, status) {
+	  	console.log('result:', result, status)
+	    if (status == 'OK') {
+	    	var point = result.routes[ 0 ].legs[ 0 ];
+	    	var eta = point.duration.text;
+	    	var updated = new Date(driver.time);
+	      document.getElementById('driverChris').innerHTML = driver.driverName + " is "+ eta + " away. Updated: " + updated.getHours() + ":" + updated.getMinutes();
+
+	    }
+	  });
+	}
+
 }
 
