68f5300f78934f976d287e0b55fffc1c2544d1a7
Chris Dermody <chrisdermody1@gmail.com>
2016-09-19 03:13:11 +0100

    console.logging out estimated journey time. legend

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,4 +1,5 @@
 var map;
+var directionsService = new google.maps.DirectionsService();
 
 var loadMap = function(){
   var mapDiv = document.getElementById('map');
@@ -14,16 +15,21 @@ var loadMap = function(){
   });
 }
 
-var store = firebase.database().ref('stores/borza');
+var store;
+var storeRef = firebase.database().ref('stores/borza');
 var drivers = firebase.database().ref('stores/borza/drivers');
 
-
+storeRef.on('value', function(snapshot) {
+  store = snapshot.val();
+  console.log(snapshot.val())
+});
 
 var watchDriver = function(){
     drivers.on('child_changed', function(data) {
       console.log(data.val());
       var driver = data.val();
       var latlng = new google.maps.LatLng(driver.position.lat, driver.position.long);
+      calcRoute(driver, store)
       updatePin(latlng)
     });
 }
@@ -36,5 +42,25 @@ var updatePin = function(latLng){
         animation: google.maps.Animation.DROP,
         position: latLng
     });
+}
+
+
+function calcRoute(driver,store) {
+	console.log('running calcRoute', driver, store);
+  var start = driver.position.lat + ", "+ driver.position.long;
+  var end = store.location.lat + ", " + store.location.long;
+  console.log('start:', start, "end:", end)
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function(result, status) {
+  	console.log('result:', result, status)
+    if (status == 'OK') {
+    	var point = result.routes[ 0 ].legs[ 0 ]
+      console.log(point.duration.text)
+    }
+  });
 }
 
