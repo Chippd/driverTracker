b4a9898168dccafece7efcffbc00ef2263a9ea5e
Chris Dermody <chrisdermody1@gmail.com>
2016-09-22 16:40:40 +0100

    updated show updated mins ago

--- js/myScripts.js
+++ js/myScripts.js
@@ -103,68 +103,70 @@ var startMagic = function(){
 	    if (status == 'OK') {
 	    	var point = result.routes[ 0 ].legs[ 0 ];
 	    	var eta = point.duration.text;
-	    	var updated = new Date(driverObj.locationObj.time);
-	      document.getElementById('driver_'+driverName).innerHTML = driverName + " is "+ eta + " away. Updated: " + updated.getHours() + ":" + updated.getMinutes();
-
+	    	// var updated = new Date(driverObj.locationObj.time);
+	    	var updated = (new Date().getTime() - driverObj.locationObj.time);
+	    	updated = Math.round(((updated % 86400000) % 3600000) / 60000);
+	      // document.getElementById('driver_'+driverName).innerHTML = driverName + " is "+ eta + " away. Updated: " + updated.getHours() + ":" + updated.getMinutes();
+	      document.getElementById('driver_'+driverName).innerHTML = driverName + " is "+ eta + " away. Updated: " + updated + " mins ago";
 	    }
 	  });
 	}
 
 }
 
-function CustomMarker(latlng, map, args) {
-	this.latlng = latlng;	
-	this.args = args;	
-	this.setMap(map);	
-}
+// function CustomMarker(latlng, map, args) {
+// 	this.latlng = latlng;	
+// 	this.args = args;	
+// 	this.setMap(map);	
+// }
 
-CustomMarker.prototype = new google.maps.OverlayView();
+// CustomMarker.prototype = new google.maps.OverlayView();
 
-CustomMarker.prototype.draw = function() {
+// CustomMarker.prototype.draw = function() {
 	
-	var self = this;
+// 	var self = this;
 	
-	var div = this.div;
+// 	var div = this.div;
 	
-	if (!div) {
+// 	if (!div) {
 	
-		div = this.div = document.createElement('div');
+// 		div = this.div = document.createElement('div');
 		
-		div.className = 'marker';
+// 		div.className = 'marker';
 		
-		div.style.position = 'absolute';
-		div.style.cursor = 'pointer';
-		div.style.width = '100px';
-		div.style.height = '35px';
-		div.style.background = 'blue';
+// 		div.style.position = 'absolute';
+// 		div.style.cursor = 'pointer';
+// 		div.style.width = '100px';
+// 		div.style.height = '35px';
+// 		div.style.background = 'blue';
 		
-		if (typeof(self.args.marker_id) !== 'undefined') {
-			div.dataset.marker_id = self.args.marker_id;
-		}
+// 		if (typeof(self.args.marker_id) !== 'undefined') {
+// 			div.dataset.marker_id = self.args.marker_id;
+// 		}
 		
-		google.maps.event.addDomListener(div, "click", function(event) {			
-			google.maps.event.trigger(self, "click");
-		});
+// 		google.maps.event.addDomListener(div, "click", function(event) {			
+// 			google.maps.event.trigger(self, "click");
+// 		});
 		
-		var panes = this.getPanes();
-		panes.overlayImage.appendChild(div);
-	}
+// 		var panes = this.getPanes();
+// 		panes.overlayImage.appendChild(div);
+// 	}
 	
-	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+// 	var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
 	
-	if (point) {
-		div.style.left = point.x + 'px';
-		div.style.top = point.y + 'px';
-	}
-};
+// 	if (point) {
+// 		div.style.left = point.x + 'px';
+// 		div.style.top = point.y + 'px';
+// 	}
+// };
 
-CustomMarker.prototype.remove = function() {
-	if (this.div) {
-		this.div.parentNode.removeChild(this.div);
-		this.div = null;
-	}	
-};
+// CustomMarker.prototype.remove = function() {
+// 	if (this.div) {
+// 		this.div.parentNode.removeChild(this.div);
+// 		this.div = null;
+// 	}	
+// };
 
-CustomMarker.prototype.getPosition = function() {
-	return this.latlng;	
-};
+// CustomMarker.prototype.getPosition = function() {
+// 	return this.latlng;	
+// };
